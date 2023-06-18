4fe35db43b6e5c62214bf3d41c84c9b76d47d4af
santhosh24444 <santhosh9mm@gmail.com>
2023-06-18 15:36:17 +0530

    0.2

--- Routes/compileroute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const languagecontroller = require('../controllers/languagecontroller');
-const router = express.Router();
-
-router.route('/').post(languagecontroller.controllan);
-
-module.exports = router;
